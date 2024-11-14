70c7dc58c50ccb688d9e7467cdf0a6fa489ebab1
benchimol2305 <cesarbenchimoldavila@gamil.com>
2024-11-13 20:12:31 -0400

    se agregan la estructura vehiculo con sus variables para el funcionamiento del codigo

--- proyecto.cpp
+++ proyecto.cpp
@@ -2,3 +2,19 @@
 #include<fstream>
 #include<sstream>
 #include<string>
+using namespace std;
+
+struct vehiculo
+{
+string modelo;
+string marca;
+string placa;
+string color;
+int year;
+int kilometraje;
+bool rentado;
+string motor;
+float precio_renta;
+string cedula_cliente;
+string fecha_entrega;
+};
