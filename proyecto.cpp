587f6e41c598f23c7d9d907f366bb2a24bd31dbc
EldiRangel <eldi456.rangel@gmail.com>
2024-11-16 11:30:57 -0800

    siguendo con el desarrollo

--- proyecto.cpp
+++ proyecto.cpp
@@ -333,7 +333,7 @@ int main() {
         cout << "7. Insertar Vehículo\n"; 
         cout << "8. Insertar Repuesto\n"; 
         cout << "9. Buscar Vehículo\n";
-
+        cout << "10. Buscar Cliente\n";
            
         cout << "12. Salir\n";
         cout << "Ingrese su opción: "; 
